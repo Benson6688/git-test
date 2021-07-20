28dfd2734750103e611d6197d88f92cac6d5e3fe
benson <benson@qq.com>
2021-07-20 20:57:12 +0800

    hot-fix first commit

--- src/main/java/com/yxb/Gdsfs.java
+++ src/main/java/com/yxb/Gdsfs.java
@@ -4,5 +4,6 @@ public class Gdsfs {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("hahaha");
+        System.out.println("sadasdasd");
     }
 }
